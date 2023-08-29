b1257c819169efdd5d403cc2594d4160f11121d5
Sandro <sandrolomidze42@gmail.com>
2023-08-29 22:57:48 +0400

    index function

--- projectOne/projectOne/settings.py
+++ projectOne/projectOne/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'app1'
+    'app1',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
