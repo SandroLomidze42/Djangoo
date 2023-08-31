b60bc0c7105309013eb34021d50594450ce5093e
Sandro <sandrolomidze42@gmail.com>
2023-08-31 15:33:47 +0400

    passing list of variables in html

--- projectOne/tasks/views.py
+++ projectOne/tasks/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 tasks=['f', 'd', 'sd']
 # Create your views here.
 def index(request):
-    render(request, "tasks/index.html", {
+    return render(request, "tasks/index.html", {
         "tasks":tasks
     })
