7d651b22967e1ea9906cbb8a9c01ba6c48e42fc9
Sandro <sandrolomidze42@gmail.com>
2023-09-02 14:32:50 +0400

    created seperate sessions for users

--- projectOne/tasks/views.py
+++ projectOne/tasks/views.py
@@ -1,24 +1,64 @@
+"""
+from django import forms 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django import forms
-tasks=['f', 'd', 'sd']
+from django.urls import reverse
+#tasks=['f', 'd', 'sd'] here tasks is global variable which is not good generally
 # Create your views here.
-class newTaskForm(forms.Form):
+class NewTaskForm(forms.Form):
     task=forms.CharField(label="new task")
-    priority=forms.IntegerField(label="Priority", min_value=1, max_value=7)
+    #priority=forms.IntegerField(label="Priority", min_value=1, max_value=5)
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
-        "tasks":tasks
+        "tasks":request.session["tasks"]
     })
+
 def add(request):
     if request.method=="POST":
-        form=newTaskForm(request.POST)
+        form=NewTaskForm(request.POST)
         if form.is_valid:
-            task=form.cleaned_data["task"]
-            tasks.append(task)
+            task=form["task"]
+            request.session["tasks"].append([task])
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
                 "form":form
             })
     return render(request, "tasks/add.html", {
-        "form":newTaskForm()
+        "form":NewTaskForm()
     })
+"""
+from django import forms
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
+
+# Create your views here.
+def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+    return render(request, "tasks/index.html", {
+        "tasks": request.session["tasks"]
+    })
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    else:
+        return render(request, "tasks/add.html", {
+            "form": NewTaskForm()
+        })
